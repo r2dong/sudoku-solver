e388074414b5cc68ef7b9efd0116303657a68879
r2dong <r2dong@yahoo.com>
2018-04-29 13:47:41 -0500

    add square to getInd, coments, modify tests

--- part2.fs
+++ part2.fs
@@ -4,9 +4,16 @@ type SudokuBoard = {
   fills: Cell list
   clues: Cell list
   size: int
+  size': int
 }
 with
-  static member construct s c =
+  (* 
+    construct a new sudoku board 
+    s': size ^ 0.5 of the board, must be integer
+    c: clues
+  *)
+  static member construct s' c =
+    let s = s' * s' in
     let f n (_, value) = if value > s then n + 1 else n in
     if List.fold f 0 c > 0
       then failwith "invalid cell value"
@@ -30,12 +37,19 @@ with
         let empties = genEmpty ((s * s) - 1) nonEmpty in
         {
           clues = c
-          size = s
           fills = empties
+          size = s
+          size' = s'
         }
+  (*
+  all cells in sorted order
+  *)
   member b.allCells = 
     let allCells = List.concat [b.clues; b.fills] in
     List.sortBy (fun (ind, _) -> ind) allCells
+  (*
+  get indicies of all cells that may attack that with index i
+  *)
   member b.getInd i =
     if i >= b.size * b.size || i < 0
       then failwith "invalid index"
@@ -44,8 +58,17 @@ with
       let col = i % b.size in
       let origin = row * b.size in
       let rows = [origin .. (origin + b.size - 1)] in
-      let cols = [for j in 0 .. 8 -> col + j * b.size]
-      List.concat [rows; cols]
+      let cols = [for j in 0 .. b.size - 1 -> col + j * b.size]
+      let row' = row / b.size' in
+      let col' = col / b.size' in
+      let upperLeft = row' * b.size' * b.size + col' * b.size' in
+      let squares = [for i in upperLeft .. upperLeft + b.size' - 1 -> 
+                      [for j in 0 .. b.size' - 1 -> i + j * b.size]
+                    ]
+      List.concat [rows; cols; List.concat squares]
+  (*
+  check if cell with index i is empty
+  *)
   member b.isFilled i =
     let rec isFilledHelp i cells =
       match cells with
@@ -57,6 +80,9 @@ with
       | [] -> failwith "index not found, please double check"
     in
     isFilledHelp i b.allCells
+  (*
+  retrieve a particular cell with index i
+  *)
   member b.getCell i =
     let rec gcHelp i ac =
       match ac with
@@ -68,6 +94,9 @@ with
         | [] -> failwith "cannot get cell, please double check"
     in
     gcHelp i b.allCells
+  (*
+  calculate attacks on cell with index i
+  *)
   member b.getCellAttacks i =
     match b.isFilled i with
       false -> 0
@@ -85,6 +114,9 @@ with
           s
       in
       List.fold accFun 0 inds
+  (*
+  print board to console
+  *)
   member b.print = 
     let rec printHelp (ac: Cell list) =
       match ac with
@@ -100,14 +132,7 @@ with
     printHelp b.allCells
 
 // tests
-
-// test getInd
-let tb = {
-  fills = []
-  size = 9
-  clues = []
-}
-let r = tb.getInd 20
+(*
 
 // test all Cells
 let tb2 = {
@@ -116,19 +141,9 @@ let tb2 = {
   size = 2
 }
 printf "%d\n" tb2.allCells.Length
-
-// test print
-let tb1 = {
-  fills = [(0, 1); (2, 2); (3, 1)]
-  clues = [(1, 2)]
-  size = 2
-}
-tb1.print
+*)
 
 // test construct
-let tb3 = SudokuBoard.construct 5 [(0, 1); (24, 5); (10, 3); (19, 4)]
-tb3.print
-
 let ws301Clues = [
   (1, 3);
   (2, 9);
@@ -154,7 +169,13 @@ let ws301Clues = [
   (77, 1);
   (80, 8);
 ]
-let ws301 = SudokuBoard.construct 9 ws301Clues
+let ws301 = SudokuBoard.construct 3 ws301Clues
 ws301.print
 
-// next: check get #attacks
+// test getInd
+ws301.getInd 54
+
+// test allCells
+ws301.allCells
+
+// test ifFilled
